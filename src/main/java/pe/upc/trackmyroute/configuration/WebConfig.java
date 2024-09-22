2f8972fac66bfd84c99de6dbe32fb9d279f7d1c7
Cesar <129460134+CesarrAnn@users.noreply.github.com>
2024-09-22 04:00:30 -0500

    Initial commit

---
+++ src/main/java/pe/upc/trackmyroute/configuration/WebConfig.java
@@ -0,0 +1,21 @@
+package pe.upc.trackmyroute.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                // Permitir CORS para todas las rutas y orígenes
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+}
